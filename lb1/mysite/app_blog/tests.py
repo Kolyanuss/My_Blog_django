a7ad4cbfd37237be3e384970baff8c8d91e06d5a
0xKolyanus <mukola.1zt.@gmail.com>
2024-03-19 13:00:01 +0200

    added test

--- lb1/mysite/app_blog/tests.py
+++ lb1/mysite/app_blog/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import HomePageView
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve(reverse('home'))
+        self.assertEqual(view.func.view_class, HomePageView)
+        
+    def test_category_view_status_code(self):
+        url = reverse('articles-category-list', args=('politics',)) 
+        response = self.client.get(url) 
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_view_status_code(self):
+        url = reverse('news-detail', args=[2024,3,19,'test'])
+        print("\n-------============------")
+        print(url)
+        response = self.client.get(url) 
+        self.assertEqual(response.status_code, 200) # why error?
+
+
+
+
